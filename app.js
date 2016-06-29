3d20c2b5e368f2ea42e4f9f1ac74d3b108d65899
Sahasha Reese <codeHer@icloud.com>
2016-06-29 08:54:33 -0500

    Files Updated Commit

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
-/ JavaScript Document
-
-// JavaScript Document
-$(document).ready(function() {
-  console.log('ready');
-  $('box one').click(function() {
-    console.log('click');
-    var widthToAnimateTo;
-    var heightToAnimateTo;
-    if( $('box two').css('width') == '100px' ){
-      widthToAnimateTo = '200';
-      heightToAnimateTo = '200';
-    } else {
-      widthToAnimateTo = '100';
-      heightToAnimateTo = '100';
-    }
-    $('box two, box three').animate({
-      width: widthToAnimateTo,
-      height: heightToAnimateTo
-    }, 5000, function() {
-        // animation complete
-        $('box three').css('background-color', 'gray');
-    });
-  });
+//Javascript
+$('div').on('click', function(){
+    $(this).toggleClass('selected');
 });
-  
