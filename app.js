fb7e528e2980d4b2795160d48ff3998ce197ce36
Sahasha Reese <codeHer@icloud.com>
2016-06-30 22:11:53 -0500

    Updating Activity Complete

--- app.js
+++ app.js
@@ -1,4 +1,51 @@
 //Javascript
-$('div').on('click', function(){
-    $(this).toggleClass('selected');
-});
+window.onload = init;
+var temp = 0;
+var level = 1;
+var guesses = 3;
+
+function init() {
+    rand();
+}
+
+function correct() {
+    document.getElementById("result").innerHTML = "You are correct! Guess another color.";
+    level++;
+    reset();
+}
+
+function incorrect() {
+    document.getElementById("result").innerHTML = "Sorry, you are incorrect.";
+    guesses--;
+    document.getElementById("guesses").innerHTML = guesses;
+    if (guesses === 0) {
+        level = 1;
+        reset();
+        document.getElementById("result").innerHTML = "Guess a color";
+    }
+
+}
+
+function reset() {
+    $(".box").animate({
+        "opacity": "1"
+    }, "slow");
+    guesses = 3;
+    temp = Math.floor((Math.random() * 6) + 1);
+    document.getElementById("level").innerHTML = level;
+    document.getElementById("guesses").innerHTML = guesses;
+}
+
+function rand() {
+    temp = Math.floor((Math.random() * 6) + 1);
+    $("div.box").click(function() {
+        if (temp == $(this).data("id")) {
+            correct();
+        } else {
+            $(this).animate({
+                "opacity": "0.25"
+            }, "slow");
+            incorrect();
+        }
+    });
+}
